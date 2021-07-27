ca0a380662987168ba075edd764150c094296cd1
Frodi1998 <rafael.abdulov.650@gmail.com>
2021-07-27 21:52:22 +0500

    delete lib directory

--- lib/context/contextHandler.d.ts
+++
@@ -1,2 +0,0 @@
-import { Context, IContext, IHandlerParams } from "../types";
-export declare function contextHandler<c extends Context>(context: c & IContext, bot: IHandlerParams): Promise<void>;
--- lib/errors/configureError.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class ConfigureError extends Error {
-    constructor(message: string);
-}
