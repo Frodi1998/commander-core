a173fd8f8fe2eb3635ef84925786622182335c8f
Frodi1998 <rafael.abdulov@gmail.com>
2021-09-30 17:32:30 +0500

    update event-emiter

--- src/core/utils/event-emiter.ts
+++ src/core/utils/event-emiter.ts
@@ -1,19 +1,4 @@
 import { EventEmitter } from 'events';
-import { UtilsCore } from '.';
-
-interface IListener {
-  context: Record<string, unknown>,
-  utils: UtilsCore,
-  error?: Error
-}
-
-interface EventListener {
-  on(event: 'command_begin', listener: (data: IListener) => void): this;
-  on(event: 'command_job', listener: (data: IListener) => void): this;
-  on(event: 'command_error', listener: (data: IListener) => void): this;
-  on(event: 'command_ready', listener: (data: IListener) => void): this;
-  on(event: 'command_not_found', listener: (data: IListener) => void): this;
-}
 
 class EventListener extends EventEmitter {}
 
