df984a403b2bd907bb0ef053c988cfb3e3b525fb
Frodi1998 <rafael.abdulov@gmail.com>
2021-10-03 13:36:22 +0500

    update execute command

--- src/core/executeCommand.ts
+++ src/core/executeCommand.ts
@@ -11,6 +11,12 @@ export default async function executeCommand<ctx extends Context>(context: ctx &
 	logger('execute param context: %O', context);
 	logger('execute param utils: %O', utils);
 
+  this.events.emit('command_begin', {context, utils});
+    
+  if(!context.$command) {
+    context.$command = context.text
+  }
+
   const startTime = Date.now();
   const command: Command = await utils.commander.find<ctx>(context);
 	
