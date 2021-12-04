edf4d5fbc59601200cc9d193f236aa5bef5bc3f0
Frodi1998 <rafael.abdulov@gmail.com>
2021-12-05 02:14:39 +0500

    fix test context

--- tests/context.ts
+++ tests/context.ts
@@ -1,7 +1,6 @@
 import { Context, IContext } from '../dist/types';
 
 export class MessageCTX extends Context implements IContext {
-  text: string;
   $command?: string;
   body?: RegExpMatchArray;
 }
