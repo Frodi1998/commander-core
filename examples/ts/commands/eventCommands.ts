a7ae75da445e1dbd4d8749c81b245fac04d58156
Frodi1998 <rafael.abdulov.650@gmail.com>
2022-12-07 23:46:26 +0500

    build: fix examples

--- examples/ts/commands/eventCommands.ts
+++ examples/ts/commands/eventCommands.ts
@@ -1,18 +1,15 @@
-//@ts-ignore
-import { MessageContext } from "vk-io";
-//@ts-ignore
-import { Command, IContext } from "commander-core";
-import Utils from "../utils";
+import { MessageContext } from 'vk-io';
+import { Command, IContext, UtilsCore } from '../../..';
+import Utils from '../utils';
 
 export default new Command({
-    pattern: /^(?:event|ивент)$/i,
-    name: 'event',
-    description: 'создание события',
+  pattern: /^(?:event|ивент)$/i,
+  name: 'event',
+  description: 'создание события',
 
-    handler(context: MessageContext & IContext, bot: Utils) {
-        const params = {context, utils: bot};
-        //@ts-ignore
-        bot.events.emit('test_command', params);
-        return;
-    }
-}) //создание своего события
+  handler(context: MessageContext & IContext, bot: Utils) {
+    const params = { context, utils: bot };
+    bot.events.emit('test_command', params);
+    return;
+  },
+}); //создание своего события
--- examples/ts/start.ts
+++ examples/ts/start.ts
@@ -1,62 +1,62 @@
-/**
- * перед использованием удалите все //@ts-ignore
- */
 //@ts-ignore
 import { VK, getRandomId, MessageContext } from 'vk-io';
-//@ts-ignore
-import { Handler, IContext } from 'commander-core';
+import { Handler, IContext } from '../..';
 import { resolve } from 'path';
 import Utils from './utils';
 
 interface IListener {
-	context: MessageContext & IContext;
-	utils: Utils;
-	error?: Error;
+  context: MessageContext & IContext;
+  utils: Utils;
+  error?: Error;
 }
 
 const vk = new VK({
-    token: '' // токен группы
-})
-
-const handler = new Handler({
-	commands: {
-		directory: resolve(__dirname, 'commands') //директория с командами
-	},
-    strictLoader: true,
-    utils: new Utils() 
-}) //создание экземпляра обработчика
-
-handler.events.on('command_error', async({context, utils, error}: IListener) =>{
-	context.send(`Произошла непредвиденная ошибка`);
-
-	if(utils.adminIds) {
-		vk.api.messages.send({
-			user_ids: utils.adminIds,
-			random_id: getRandomId(), 
-			//@ts-ignore
-			message: `Ошибка в команде ${utils.getCommand.name}: 
-				${context.senderId} => ${context.command}
-				${error.stack}`
-		}) 
-	}
-}) // событие срабатывает при ошибке в командах и отправляет текст ошибки в лс разработчику
-
-handler.events.on('command_not_found', async({context}: IListener) =>{
-	if(!context.isChat) {
-		context.send(`Введенной вами команды не существует!`)
-	} 
+  token: '', // токен группы
 });
 
-handler.loadCommands()
-.then(() => console.log('commands loaded')) //загружает команды
-.catch((err: Error) => console.error(err)) //обработайте ошибку
+const handler = new Handler({
+  commands: {
+    directory: resolve(__dirname, 'commands'), //директория с командами
+  },
+  strictLoader: true,
+  utils: new Utils(),
+}); //создание экземпляра обработчика
 
-vk.updates.on('message_new', async(context) => {
-    if(context.isGroup) return; //проверка на бота
+handler.events.on(
+  'command_error',
+  async ({ context, utils, error }: IListener) => {
+    context.send(`Произошла непредвиденная ошибка`);
 
-    if(context.text) context.text = context.text.replace(/^\[club(\d+)\|(.*)\]/i, '').trim(); //удаляет упоминание
+    if (utils.adminIds) {
+      vk.api.messages.send({
+        user_ids: utils.adminIds,
+        random_id: getRandomId(),
+        message: `Ошибка в команде ${utils.getCommand?.name}: 
+				${context.senderId} => ${context.command}
+				${error?.stack}`,
+      });
+    }
+  },
+); // событие срабатывает при ошибке в командах и отправляет текст ошибки в лс разработчику
 
-	await handler.execute(context); //отправляет объект сообщения в обработчик
+handler.events.on('command_not_found', async ({ context }: IListener) => {
+  if (!context.isChat) {
+    context.send(`Введенной вами команды не существует!`);
+  }
+});
+
+handler
+  .loadCommands()
+  .then(() => console.log('commands loaded')) //загружает команды
+  .catch((err: Error) => console.error(err)); //обработайте ошибку
+
+vk.updates.on('message_new', async context => {
+  if (context.isGroup) return; //проверка на бота
+
+  if (context.text)
+    context.text = context.text.replace(/^\[club(\d+)\|(.*)\]/i, '').trim(); //удаляет упоминание
+
+  await handler.execute(context); //отправляет объект сообщения в обработчик
 });
 
 vk.updates.start().then(() => console.log('start'));
