e53aaece9db2b2f9c2a93d1f61daa36f6f8299e5
Frodi1998 <rafael.abdulov.650@gmail.com>
2024-03-02 12:27:48 +0500

    fix: fix Constructable type

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,7 @@ export type AnyObject = Record<string, any>;
 export type UnknownObj = Record<string, unknown>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type Constructable<T> = new (...args: any) => T;
+export type Constructable<T> = new (...args: any[]) => T;
 
 export type AssertExtendedType<T, T1> = T extends T1 ? T1 : T & T1;
 
